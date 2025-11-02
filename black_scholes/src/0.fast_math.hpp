5402993a6a2ea87aa9a626084aaca7099e92818c
Léon Bourlon <leonbourlon@hotmail.com>
2025-11-02 22:45:40 +0100

    more work fully independent of glibc

--- black_scholes/src/0.fast_math.hpp
+++ black_scholes/src/0.fast_math.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstdint>
 #include <numbers>
+#include <array>
 
 using std::numbers::e;
 using std::numbers::ln2;
@@ -13,36 +14,45 @@ constexpr double sqrt_2pi = 2.5066282746310002;
 
 namespace fm {
 
-inline double sqrt(double x) { return std::sqrt(x); }
-inline double ln(double x) { return std::log(x); }
-
 constexpr double p2(double x) { return x * x; }
 constexpr double p3(double x) { return x * x * x; }
 
-constexpr double pad_exp(double x);//imprecise
+constexpr double pad_exp(double x); //imprecise
 constexpr double exp_taylor_expansion(double x);
+constexpr double exp_continued_fraction(double x);
 constexpr double exp_polynomial(double x);
+constexpr double exp_test(double x);
 constexpr double exp(double x) { return std::exp(x); }
+// constexpr double exp(double x) { return std::exp(x); }
 
-constexpr double N_taylor_expansion(double x); // slowest
-constexpr double N_marsaglia(double x); // close second
+
+// Newton-Halley method
+inline double ln_fracs(double x) {
+    constexpr int elements = 2; // converges insanely fast
+    double yn = x;
+    double yn1 = 0;
+    for (int i = 0; i < elements; i += 1) {
+        yn1 = yn + 2 *(x-fm::exp(yn))/(x+fm::exp(yn));
+        yn = yn1;
+    }
+    return yn1;
+}
+// inline double ln(double x) { return std::log(x); }
+inline double ln(double x) { return ln_fracs(x); }
+
+constexpr double sqrt_expln(double x) { return fm::exp(fm::ln(x)/2.);  }
+constexpr double sqrt_pln(double x) { return std::pow(10, ln(x))/2.; }
+inline double sqrt(double x) {return sqrt_expln(x);}
+
+constexpr double N_marsaglia(double x); // slowest
+constexpr double N_taylor_expansion(double x); // close second
 constexpr double N_zelen_severo(double x); // fastest
-// Normal distribution implementation
 constexpr double N(double x) { return N_zelen_severo(x); }
 
 
-// shore phi - 1
-inline double shore_inverse_phi(double x) {
-    if (x < 0.5) return -shore_inverse_phi(1 - x);
-    return 1 / 5.5556 * (1 - std::pow(((1 - x) / x), 0.1186));
-}
+constexpr double pow_i(double val, uint16_t exponent);
 
-constexpr double pow_i(double val, uint16_t exponent) {
-    double sum = val;
-    for (uint16_t e = 1; e < exponent; ++e)
-        sum *= val;
-    return sum;
-}
+/* ----------- Normal Distribution funcs ----------- */
 
 // Standard normal probability density function
 // Small phi not to be confused with Big Phi (which I'm notating as N);
@@ -92,16 +102,16 @@ constexpr double N_zelen_severo(double x) {
 /* ----------- EXP functions ----------- */
 
 // https://mathworld.wolfram.com/PadeApproximant.html
-constexpr double pad_exp(double x) {
+constexpr double pade_exp(double x) {
     using fm::p2;
     using fm::p3;
-    return  (120 + 60*x + 12* p2(x) + p3(x))/(120 - 60*x + 12* p2(x) - p3(x)) ;// e_3_3
+    return  (120 + 60*x + 12* p2(x) + p3(x))
+            /(120 - 60*x + 12* p2(x) - p3(x)) ;// e_3_3
 }
 
 // Taylor expansion exponential
 // Not considerably slower than std::exp()
 // but is actually constexpr ( c++26 not out :/ )
-// plus we can specify its precision
 constexpr double exp_taylor_expansion(double x) {
     constexpr int elements = 16;
     double sum = 1., factorial = 1., power = 1;
@@ -110,31 +120,64 @@ constexpr double exp_taylor_expansion(double x) {
 }
 
 
+constexpr double _cnt_frac_recurse(double x, int i) {
+    constexpr int step = 4;
+    constexpr int elements = step* 5;
+    i += step;
+    if (i < elements){
+        return (i) + p2(x) / _cnt_frac_recurse(x, i);
+    } else {
+        return x;
+    }
+}
+//
+// Continued fraction based on Euler Identity
+constexpr double exp_continued_fraction(double x) {
+    return 1 + 2*x / ((2-x) + p2(x) /_cnt_frac_recurse(x, 2));
+}
+
+
 
 // Yaya, D Dia (2023) Approximate incomplete integrals, application to
 // complementary error function
 // relative error of less than 2^-53
-// for x > 0
-// constexpr double erfc(double x) {
-//     constexpr const double b0_0 = 2.92678600515804815402;
-//     constexpr std::array<double, 5> b1 = {
-//         8.97280659046817350354, 10.27157061171363078863, 12.72323261907760928036,
-//         16.88639562007936907786, 24.12333774572479110372};
-//     constexpr std::array<double, 5> b2 = {
-//         5.81582518933527390512, 5.70347935898051436684, 5.51862483025707963145,
-//         5.26184239579604207321, 4.92081346632882032881};
-//     constexpr std::array<double, 5> c1 = {
-//         11.61511226260603247078, 18.25323235347346524796, 18.38871225773938486923,
-//         18.61193318971775795045, 24.14804072812762821134};
-//     constexpr std::array<double, 5> c2 = {
-//         3.83362947800146179416, 7.30756258553673541139, 8.42742300458043240405,
-//         5.66479518878470764762, 4.91396098895240075156};
-//
-//     double sum = 1;
-//     for (int i = 0; i < c2.size(); ++i) {
-//         sum *= ((fm::p2(x) + c2[i] * x + c1[i]) / (fm::p2(x) + b2[i] * x + b1[i]));
-//     }
-//     return ( sum / (x + b0_0));
-// }
+// expects x > 0
+constexpr double erfc(double x) {
+    constexpr const double b0_0 = 2.92678600515804815402;
+    constexpr std::array<double, 5> b1 = {
+        8.97280659046817350354, 10.27157061171363078863, 12.72323261907760928036,
+        16.88639562007936907786, 24.12333774572479110372};
+    constexpr std::array<double, 5> b2 = {
+        5.81582518933527390512, 5.70347935898051436684, 5.51862483025707963145,
+        5.26184239579604207321, 4.92081346632882032881};
+    constexpr std::array<double, 5> c1 = {
+        11.61511226260603247078, 18.25323235347346524796, 18.38871225773938486923,
+        18.61193318971775795045, 24.14804072812762821134};
+    constexpr std::array<double, 5> c2 = {
+        3.83362947800146179416, 7.30756258553673541139, 8.42742300458043240405,
+        5.66479518878470764762, 4.91396098895240075156};
+
+    double sum = 1;
+    for (int i = 0; i < c2.size(); ++i) {
+        sum *= ((fm::p2(x) + c2[i] * x + c1[i]) / (fm::p2(x) + b2[i] * x + b1[i]));
+    }
+    return ( sum / (x + b0_0));
+}
+
+/* ----------- Misc ----------- */
+
+// Shore's inverse phi
+constexpr double inverse_phi_shore(double x) {
+    if (x < 0.5) return -inverse_phi_shore(1 - x);
+    return 1 / 5.5556 * (1 - std::pow(((1 - x) / x), 0.1186));
+}
+
+
+constexpr double pow_i(double val, uint16_t exponent) {
+    double sum = val;
+    for (uint16_t e = 1; e < exponent; ++e)
+        sum *= val;
+    return sum;
+}
 
 } // namespace fm
